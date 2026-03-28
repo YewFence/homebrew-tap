8eefd088f518e067cec038b67c1a302cb30bbfc5
YewFence <hello@yewfence.dev>
2026-03-28 16:53:17 +0000

    fix: 修复 sshm formula 的 brew style 问题

--- Formula/sshm.rb
+++ Formula/sshm.rb
@@ -1,6 +1,7 @@
 # typed: false
 # frozen_string_literal: true
 
+# Formula for sshm
 class Sshm < Formula
   desc "CLI to manage ~/.ssh/config easily"
   homepage "https://github.com/YewFence/ssh-config-manager"
@@ -12,7 +13,7 @@ class Sshm < Formula
       url "https://github.com/YewFence/ssh-config-manager/releases/download/v#{version}/sshm-v#{version}-macos-arm64"
       sha256 "cebae480473a4219e942a5f0be19bcf4defa388e06ebe0f2ec526594499939be"
 
-      def install
+      define_method(:install) do
         bin.install "sshm-v#{version}-macos-arm64" => "sshm"
       end
     end
@@ -20,7 +21,7 @@ class Sshm < Formula
       url "https://github.com/YewFence/ssh-config-manager/releases/download/v#{version}/sshm-v#{version}-macos-amd64"
       sha256 "b9dcb29ee7356e1da3e05841c2a7ab4bd0ffdf91753a1565d30eeb024f698c0e"
 
-      def install
+      define_method(:install) do
         bin.install "sshm-v#{version}-macos-amd64" => "sshm"
       end
     end
@@ -30,7 +31,7 @@ class Sshm < Formula
       url "https://github.com/YewFence/ssh-config-manager/releases/download/v#{version}/sshm-v#{version}-linux-arm64"
       sha256 "2e465e0df23d0654f7b6ae6ea9b37ca9114bd427d5d3031e90bb27daa032c229"
 
-      def install
+      define_method(:install) do
         bin.install "sshm-v#{version}-linux-arm64" => "sshm"
       end
     end
@@ -38,7 +39,7 @@ class Sshm < Formula
       url "https://github.com/YewFence/ssh-config-manager/releases/download/v#{version}/sshm-v#{version}-linux-amd64"
       sha256 "a116b1335c80fca8f2d1ad4282db1e084ddbdad6d73bf7bc7d714fd9d5c1ca1f"
 
-      def install
+      define_method(:install) do
         bin.install "sshm-v#{version}-linux-amd64" => "sshm"
       end
     end
