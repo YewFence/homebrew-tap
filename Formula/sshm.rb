b857652f8fdd8270bc1d9789647465720227f955
YewFence <hello@yewfence.dev>
2026-03-29 14:31:37 +0000

    chore: bump sshm to v0.0.5

--- Formula/sshm.rb
+++ Formula/sshm.rb
@@ -5,13 +5,13 @@
 class Sshm < Formula
   desc "CLI to manage ~/.ssh/config easily"
   homepage "https://github.com/YewFence/ssh-config-manager"
-  version "0.0.4"
+  version "0.0.5"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/YewFence/ssh-config-manager/releases/download/v#{version}/sshm-v#{version}-macos-arm64"
-      sha256 "e8465bfb94d661b212fddb01d5fae8f7c9052251976e34c82c131475e17bb593"
+      sha256 "15aa8dbf6cd959969e00a501dbc3fe9111ed6083256ddb8849a8e5e42be37608"
 
       define_method(:install) do
         bin.install "sshm-v#{version}-macos-arm64" => "sshm"
@@ -19,7 +19,7 @@ class Sshm < Formula
     end
     on_intel do
       url "https://github.com/YewFence/ssh-config-manager/releases/download/v#{version}/sshm-v#{version}-macos-amd64"
-      sha256 "03c4d69fd5249448f624b66ab32e8525a5674085353769266c0043959530d507"
+      sha256 "3ea79579250098e3d36567d2346bacd15d29a2054a397620c54737a557608611"
 
       define_method(:install) do
         bin.install "sshm-v#{version}-macos-amd64" => "sshm"
@@ -30,7 +30,7 @@ class Sshm < Formula
   on_linux do
     on_arm do
       url "https://github.com/YewFence/ssh-config-manager/releases/download/v#{version}/sshm-v#{version}-linux-arm64"
-      sha256 "2c51e81473837562aad5c68f9b10b09534c341f023533bfc5333e59079a0e3e3"
+      sha256 "e8bb68c708bb841cbc0eac42cd3f9ad182f0bbde74939ec5f1a4bc1fab7f4b81"
 
       define_method(:install) do
         bin.install "sshm-v#{version}-linux-arm64" => "sshm"
@@ -38,7 +38,7 @@ class Sshm < Formula
     end
     on_intel do
       url "https://github.com/YewFence/ssh-config-manager/releases/download/v#{version}/sshm-v#{version}-linux-amd64"
-      sha256 "0e88d09b804085304836b1fa5262b9076bdc67f81a1efaf71e3f60638ee27a2a"
+      sha256 "54198f8e7e0b7482e753e69311aa016ffaf05ffb6871606da98cdc58ed932d03"
 
       define_method(:install) do
         bin.install "sshm-v#{version}-linux-amd64" => "sshm"
